ec69d4c0753fdc40f6bff700e39ee35ddafcc0f4
Priyanshi Chauhan <priyanshichauhan0707@gmail.com>
2022-10-07 19:50:29 +0530

    added condition in build failure

--- build.sh
+++ build.sh
@@ -6,6 +6,8 @@ BUILD_REPOSITORY_TAG=`getRepositoryTag`
 logInfoMessage "I'll check the docker image layers for ${COMPONENT_NAME} of tag ${BUILD_REPOSITORY_TAG}"
 sleep  $SLEEP_DURATION
 
+$VALIDATION_FAILURE_ACTION
+
 LAYERS=$(docker inspect ${COMPONENT_NAME}:${BUILD_REPOSITORY_TAG} | jq .[].RootFS.Layers | wc -l)
 IMAGE_LAYER=$(expr $LAYERS - 2)
 
@@ -15,12 +17,20 @@ logInfoMessage "Number of Layers are allowed is $MAX_ALLOWED_IMAGE_LAYERS"
 if [[ $IMAGE_LAYER -gt $MAX_ALLOWED_IMAGE_LAYERS ]]
 then
    	generateOutput IMAGE_LAYER_VALIDATOR false "Build failed please check!!!!!"
-    logErrorMessage "Number of layers are more then expected layers count"
-    logErrorMessage "build unsucessfull"
-    exit 1
-else
-	generateOutput IMAGE_LAYER_VALIDATOR true "Congratulations build succeeded!!!"
-	logInfoMessage "Number of layers in docker image is  under expected layers count"
-	logInfoMessage "build sucessfull"
-fi
+   if [[ $VALIDATION_FAILURE_ACTION == "FAILURE" ]]
+   then
+        logErrorMessage "Number of layers are more then expected layers count"
+        logErrorMessage "build unsucessfull"
+        exit 1
 
+   else
+        logWarningMessage "Number of layers are more then expected layers count please check!!!!!"
+   fi
+else
+        generateOutput IMAGE_LAYER_VALIDATOR true "Congratulations build succeeded!!!"
+        logInfoMessage "Number of layers in docker image is  under expected layers count"
+        logInfoMessage "build sucessfull"
+fi
+ 
+      	
+     
